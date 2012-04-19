4afb531d1b76d394600b3b4365934f3deea1ca3e
Joel Caballero <joel@meekgeek.net>
2012-04-18 17:57:27 -0700

    Added launch feature as well as compc

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var prompt = require('prompt');
 var program = require('commander');
 var execFile = require('child_process').execFile;
+var exec = require('child_process').exec;
 var nconf = require('nconf');
 var src_file;
 
@@ -15,12 +16,8 @@ program
 program
 	.command('init')
 	.description('Initializes and creates the mxmlc.properties file.')
-	/*.option('-k, --sdk <path>', "Directory to FLEX SDKs")
-	.option('-s, --source-path', "Source path")
-	.option('-o, --output-path', "Output path")
-	.option('-f, --source-file', "File to compilet")
-	.option('-l, --lib-path', "Add lib path")*/
 	.action( function(){
+
 		if( ! nconf.get('sdkDir') )
 			nconf.set('sdkDir', '');
 		
@@ -28,13 +25,13 @@ program
 			nconf.set('sourcePath', '');
 
 		if( ! nconf.get('sourceFile') )
-			nconf.set('sourceFile', 'Main');
+			nconf.set('sourceFile', 'Main.as');
 
 		if( ! nconf.get('libPath') )
 			nconf.set('libPath', "");
 
 		if( ! nconf.get('defaults:background-color') )
-			nconf.set('defaults:background-color', '0xffffff' );
+			nconf.set('defaults:background-color', '0x000000' );
 
 		if( ! nconf.get('defaults:frame-rate') )
 			nconf.set('defaults:frame-rate', '30' );
@@ -43,6 +40,48 @@ program
 			nconf.set('defaults:size', '980,590' );
 		
 		nconf.save(function(err){
+			process.exit(0);
+		});
+	});
+
+program
+	.command('compc')
+	.description('Compile swc')
+	.action( function(env) {
+		var file = nconf.get('sourceFile');
+		var compc = "'"+nconf.get('sdkDir')+"/bin/compc'";
+		var output = Boolean(!program.releaseBuild) ? "bin/"+file+".swc" : "release/"+file+".swc"
+		var compc_args = ["-output "+output];
+
+		if( nconf.get('sourcePath') ) {
+			compc_args.push("-include-sources "+nconf.get('sourcePath') );
+		}
+
+		if( nconf.get('libPath') ) {
+			compc_args.push("-compiler.library-path "+nconf.get('libPath') );
+		}
+
+		console.log( compc+" "+compc_args.toString().replace(',', ' ') );
+		exec(compc+" "+compc_args.toString().replace(',', ' '),function(err,stdout,stderr){
+			if(err){console.log(err);}
+			console.log('Done!'.green);
+			process.exit(0);
+		});
+	});
+
+program
+	.command('launch')
+	.description('launches swf file.')
+	.option('-b, --browser', "Launches in browser.")
+	.action( function(options) {
+		console.log(Boolean(options.browser));
+		var command = Boolean(options.browser) ? "open -a 'Google Chrome' " : "open -a 'Flash Player' ";
+		var file = nconf.get('sourceFile');
+		var swf = Boolean(!program.releaseBuild) ? "bin/"+file+".swf" : "release/"+file+".swf";
+			swf = process.cwd()+"/"+swf;
+
+		exec(command+swf,function(err,stdout,stderr) {
+			if(err){console.log(err);}
 			process.exit(0);
 		});
 	});
@@ -57,25 +96,31 @@ program
 program.parse(process.argv);
 
 
-function build(file) {
-	
-	/*"-default-background-color=0xffffff",
-	"-default-frame-rate=30",
-	"-default-size=980,590",*/
+if( ! nconf.get('sdkDir') ) {
+	console.log('No SDK detected. Run "init" first, then edit the generated mxmlc.properties file'.red);
+	process.exit(0);
+}
 
-	var mxmlc = "/Applications/Adobe Flash Builder 4.5/sdks/flex_sdk_4.6.0.23201B/bin/mxmlc";
-	var output = Boolean(!program.releaseBuild) ? "bin/"+file+".swf" : "release/"+file+".swf"
+if( ! program.args[0] ) {
+	//console.log('build');
+	build( nconf.get('sourceFile') );
+}
+
+function build(file) {
+
+	var mxmlc = nconf.get('sdkDir')+"/bin/mxmlc";
+	var output_file = file.replace(/.as$/gi, "")+".swf";
+	var output = Boolean(!program.releaseBuild) ? "bin/"+output_file : "release/"+output_file
 	var mxmlc_args = [	"-compiler.incremental=true",
 						"-compiler.include-libraries="+nconf.get('libPath'),
 						"-static-link-runtime-shared-libraries=true",
 						"-debug="+Boolean(!program.releaseBuild),
 						"-source-path="+nconf.get('sourcePath'),
-						process.cwd()+"/"+file+".as",
+						process.cwd()+"/"+file,
 						"-o="+output ]
 
 	var defaults = nconf.get('defaults');
 	for ( var key in defaults ) {
-		console.log( key, defaults[key] );
 		mxmlc_args.push("-default-"+key+"="+defaults[key]);
 	}
 	
@@ -85,23 +130,3 @@ function build(file) {
 		process.exit(0);
 	})
 };
-
-//console.log(process.env);
-if( ! nconf.get('sdkDir') ) {
-	console.log('No SDK detected. Run "init" first, then edit the generated mxmlc.properties file'.red);
-	process.exit(0);
-}
-
-//console.log("name: ",program.name);
-//console.log(program.args[0]);
-
-if( ! program.args[0] ) {
-	//console.log('build');
-	build( nconf.get('sourceFile') );
-}
-
-//console.log('program.command', program );
-//if(!src_file){
-//	build(nconf.get('source-path'));
-//}
-
